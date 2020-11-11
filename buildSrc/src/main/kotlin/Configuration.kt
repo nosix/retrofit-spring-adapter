51439a040eb624f5da0a98076d87e0b434c3b7c6
nosix <nosix@users.noreply.github.com>
2020-11-11 11:10:36 +0900

    Change version format
    The format is {SpringBootVersion}.{RetrofitVersion}.{patchVersion}

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,9 +1,17 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 object Configuration {
     object Versions {
         const val kotlin = "1.4.10"
         const val spring = "5.3.0-RC2"
         const val springBoot = "2.4.0-M4"
         const val retrofit = "2.9.0"
-        const val current = "0.0.1-SNAPSHOT"
+
+        const val patch = "0"
+
+        val current = "${springBoot.toPart()}.${retrofit.toPart()}.$patch"
+
+        private fun String.toPart(): String =
+            this.split(".", "-").take(2).joinToString(separator = "") { "%02d".format(it.toInt()) }
     }
 }
