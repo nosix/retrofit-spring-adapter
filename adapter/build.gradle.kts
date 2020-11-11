a1029065b5fba7538cbeb1c8854562f022867535
nosix <nosix@users.noreply.github.com>
2020-11-11 12:11:37 +0900

    Publish in the release directory
    Because GitHub Packages requires authentication.

--- adapter/build.gradle.kts
+++ adapter/build.gradle.kts
@@ -54,12 +54,8 @@ publishing {
 	}
 	repositories {
 		maven {
-			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/nosix/retrofit-spring-adapter")
-			credentials {
-				username = System.getenv("GITHUB_ACTOR")
-				password = System.getenv("GITHUB_TOKEN")
-			}
+			name = "ReleaseDir"
+			url = uri("file:${rootProject.projectDir}/release")
 		}
 	}
 }
